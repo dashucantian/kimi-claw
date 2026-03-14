ffb4f339cb8f3532effa202c4bbe796e93195ab0
宗国法师 <zongguo@wulingzen.org>
2026-03-14 14:06:29 +0800

    Initial commit: 知止小程序 v28

---
+++ app.js
@@ -0,0 +1,24 @@
+App({
+  globalData: {
+    userInfo: null,
+    settings: {
+      duration: 3,
+      audioEnabled: true,
+      reminderTime: '21:00'
+    }
+  },
+
+  onLaunch() {
+    // 加载本地设置
+    const settings = wx.getStorageSync('zhiguan_settings')
+    if (settings) {
+      this.globalData.settings = { ...this.globalData.settings, ...settings }
+    }
+    
+    // 加载练习记录
+    const records = wx.getStorageSync('zhiguan_records')
+    if (!records) {
+      wx.setStorageSync('zhiguan_records', [])
+    }
+  }
+})
